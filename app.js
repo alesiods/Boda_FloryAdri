2c4d509c58bc34f98c576cd559ab53fa923c9970
alesiods <alesioadrover@gmail.com>
2022-02-10 20:52:46 -0300

    Cambio horario

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ simplyCountdown('#cuenta', {
     year: 2022, // required
     month: 2, // required
     day: 27, // required
-    hours: 20, // Default is 0 [0-23] integer
-    minutes: 30, // Default is 0 [0-59] integer
+    hours: 18, // Default is 0 [0-23] integer
+    minutes: 15, // Default is 0 [0-59] integer
     seconds: 0, // Default is 0 [0-59] integer
     words: { //words displayed into the countdown
         days: { singular: 'Día', plural: 'Días' },
